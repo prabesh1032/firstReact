5ac9e700d563505df745d0ad3dfec84e7f10e63a
prabesh <praveshach1032@gmail.com>
2025-05-06 16:06:48 +0545

    basic concept

---
+++ src/screens/Register2.js
@@ -0,0 +1,39 @@
+import React from 'react'
+
+function TodoList() {
+  const [todos, setTodos] = React.useState([]);
+  const [input, setInput] = React.useState("");
+
+  const addTodo = () => {
+    if (input.trim()) {
+      setTodos([...todos, input]);
+      setInput("");
+    }
+  };
+
+  const removeTodo = (index) => {
+    setTodos(todos.filter((_, i) => i !== index));
+  };
+
+  return (
+    <div>
+      <h1>Todo List</h1>
+      <input
+        type="text"
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+        placeholder="Add a new todo"
+      />
+      <button onClick={addTodo}>Add</button>
+      <ul>
+        {todos.map((todo, index) => (
+          <li key={index}>
+            {todo} <button onClick={() => removeTodo(index)}>Removes</button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default TodoList;
